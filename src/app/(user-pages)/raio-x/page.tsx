0051bef073a694a6115ec34cd8521ce87037f547
Tpz64 <thaispz64@gmail.com>
2024-10-25 11:08:41 -0300

    responsividade iframe

--- src/app/(user-pages)/raio-x/page.tsx
+++ src/app/(user-pages)/raio-x/page.tsx
@@ -2,8 +2,8 @@ const RaioX = () => {
     return (
       <div className="relative w-full bg-sienna flex items-center justify-center">
         <div className="relative max-h-fit min-h-screen bg-white md:w-[80%] xl:w-1/2">
-            <div className="relative w-auto flex items-center justify-center">
-                <iframe width="609" height="850" src="https://embed.figma.com/proto/ORhch2zJbBJVx12008XNhB/Raio-X?node-id=1-2&node-type=canvas&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A2&embed-host=share" allowFullScreen></iframe>
+            <div className="relative w-full flex items-center justify-center">
+                <iframe className="w-full h-[450px] sm:h-[600px] md:w-[609px] md:h-[850px]" src="https://embed.figma.com/proto/ORhch2zJbBJVx12008XNhB/Raio-X?node-id=1-2&node-type=canvas&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A2&embed-host=share" allowFullScreen></iframe>
             </div>           
         </div>
       </div>
