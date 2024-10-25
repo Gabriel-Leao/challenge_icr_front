b8da61253dce9a2458d52451a23ba24b9e548eb6
Gabriel-Leao <gabriel.leao2507@gmail.com>
2024-10-25 13:03:41 -0300

    fix: historias

---
+++ src/app/(user-pages)/historias/sangue/page.tsx
@@ -0,0 +1,16 @@
+const Sangue = () => {
+  return (
+    <div className="relative w-full bg-sienna flex items-center justify-center">
+      <div className="relative max-h-fit min-h-screen bg-white md:w-[80%] xl:w-1/2">
+        <div className="relative w-full flex items-center justify-center">
+          <iframe
+            className="w-full h-[450px] sm:h-[600px] md:w-[609px] md:h-[850px]"
+            src="https://embed.figma.com/proto/NofcuCcG7bRZr2wN2iixqP/Exame-de-Sangue?node-id=1-2&node-type=canvas&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A2&embed-host=share"
+            allowFullScreen></iframe>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sangue
--- src/components/BookCover/index.tsx
+++ src/components/BookCover/index.tsx
@@ -2,23 +2,23 @@ import Link from 'next/link'
 import defaultImg from '/public/default_img.png'
 import Image from 'next/image'
 
-const BookCover = ({ cover }: { cover?: string | null }) => {
+const getHistoryLink = (cover: string) => {
+  switch (cover) {
+    case 'https://i.ibb.co/RH09DfY/Captura-de-tela-de-2024-05-21-18-18-06.png':
+      return '/historias/raio-x'
+    case 'https://i.ibb.co/qrFcfWL/Captura-de-tela-de-2024-05-21-18-17-32.png':
+      return '/historias/hemodialise'
+    case 'https://i.ibb.co/2SwwCtK/Captura-de-tela-de-2024-05-21-18-18-24.png':
+      return 'historias/sangue'
+    default:
+      return '/historias'
+  }
+}
 
-  const getLink = (cover: string | null | undefined) => {
-    switch (cover) {
-      case 'https://i.ibb.co/RH09DfY/Captura-de-tela-de-2024-05-21-18-18-06.png':
-        return '/raio-x'; 
-      case 'https://i.ibb.co/qrFcfWL/Captura-de-tela-de-2024-05-21-18-17-32.png':
-        return '/hemodialise'; 
-      case 'https://i.ibb.co/2SwwCtK/Captura-de-tela-de-2024-05-21-18-18-24.png':
-        return '/sangue'; 
-      default:
-        return '/bookpage'; 
-    }
-  };
+const BookCover = ({ cover }: { cover?: string | null }) => {
   return (
     <>
-      <Link href={getLink(cover)}>
+      <Link href={getHistoryLink(cover!)}>
         <Image
           src={cover || defaultImg}
           alt="Capa padrãoo"
@@ -26,10 +26,7 @@ const BookCover = ({ cover }: { cover?: string | null }) => {
           width={334}
           height={499}
         />
-
       </Link>
-
-      
     </>
   )
 }
