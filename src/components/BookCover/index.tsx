b05afa3e1777d07589b52c0fd3d6677f1d99eb2c
Tpz64 <thaispz64@gmail.com>
2024-10-24 23:44:25 -0300

    Adicionando as paginas dos livros individuais

--- src/components/BookCover/index.tsx
+++ src/components/BookCover/index.tsx
@@ -3,9 +3,22 @@ import defaultImg from '/public/default_img.png'
 import Image from 'next/image'
 
 const BookCover = ({ cover }: { cover?: string | null }) => {
+
+  const getLink = (cover: string | null | undefined) => {
+    switch (cover) {
+      case 'https://i.ibb.co/RH09DfY/Captura-de-tela-de-2024-05-21-18-18-06.png':
+        return '/raio-x'; 
+      case 'https://i.ibb.co/qrFcfWL/Captura-de-tela-de-2024-05-21-18-17-32.png':
+        return '/hemodialise'; 
+      case 'https://i.ibb.co/2SwwCtK/Captura-de-tela-de-2024-05-21-18-18-24.png':
+        return '/sangue'; 
+      default:
+        return '/bookpage'; 
+    }
+  };
   return (
     <>
-      <Link href="/bookpage">
+      <Link href={getLink(cover)}>
         <Image
           src={cover || defaultImg}
           alt="Capa padrãoo"
@@ -13,7 +26,9 @@ const BookCover = ({ cover }: { cover?: string | null }) => {
           width={334}
           height={499}
         />
+
       </Link>
+
       
     </>
   )
